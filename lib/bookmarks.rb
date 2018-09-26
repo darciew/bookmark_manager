028a9230878461743a57660683ba90a21a5ec682
Darcie <darciewalsh@hotmail.com>
2018-09-26 18:06:06 +0100

    make valid_url method private

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -22,6 +22,8 @@ class Bookmarks
     con.exec("INSERT INTO bookmarks (url) VALUES ('#{url}')")
   end
 
+  private
+
   def self.valid_url?(url)
     url_regexp = /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
     url =~ url_regexp ? true : false
